f1b0f4dbf3902484601f10fc2aa6a630f30d7932
Gregory John Casamento <greg.casamento@gmail.com>
2026-05-28 05:32:30 -0400

    Add an usable browser that demonstrates how to use the WebView from a .gorm based app.

---
+++ Applications/NetStep/AppController.h
@@ -0,0 +1,43 @@
+/* 
+   Project: WebStep
+
+   Author: Gregory John Casamento
+
+   Created: 2026-05-28 04:32:27 -0400 by heron
+   
+   Application Controller
+*/
+ 
+#ifndef _PCAPPPROJ_APPCONTROLLER_H
+#define _PCAPPPROJ_APPCONTROLLER_H
+
+#import <AppKit/AppKit.h>
+// Uncomment if your application is Renaissance-based
+//#import <Renaissance/Renaissance.h>
+
+@interface AppController : NSObject
+{
+  IBOutlet NSWindow *_window;
+}
+
+// Class methods...
++ (void)  initialize;
+
+// Initialization
+- (id) init;
+- (void) dealloc;
+- (void) awakeFromNib;
+
+// Notification methods...
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif;
+- (BOOL) applicationShouldTerminate: (id)sender;
+- (void) applicationWillTerminate: (NSNotification *)aNotif;
+- (BOOL) application: (NSApplication *)application
+	    openFile: (NSString *)fileName;
+
+// Actions...
+- (IBAction) showPrefPanel: (id)sender;
+
+@end
+
+#endif
